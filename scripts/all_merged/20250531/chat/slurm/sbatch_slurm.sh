de331f24a5466d47411574cc7d9143773e2ac222
sphilmoon <sphilmoon@gmail.com>
2025-06-01 09:34:45 +0100

    testing new merging approach

---
+++ scripts/all_merged/20250531/chat/slurm/sbatch_slurm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=dge_cht                  # Job name
+#SBATCH --nodes=1                           # Number of nodes
+#SBATCH --output=seurat_%j.out              # Output log file (%j = job ID)
+#SBATCH --error=seurat_%j.err               # Error log file
+
+# set working directory
+cd /mnt/lustre/RDS-live/moon/ephemeral/NK_project/outputs
+
+# load docker in the HPC
+module load docker
+
+# define paths
+HOST_DATA_DIR="/mnt/lustre/RDS-live/moon/ephemeral/NK_project"
+CONTAINER_DATA_DIR="/home"
+R_SCRIPT="/scripts/all_merged/20250531/chat/merge_sct_cluster_dge_chat.r"
+
+# run the docker container with the R script with chat gpt.
+docker run --rm \
+    -v ${HOST_DATA_DIR}:${CONTAINER_DATA_DIR} \
+    satijalab/seurat:5.0.0 \
+    Rscript ${CONTAINER_DATA_DIR}/${R_SCRIPT}
