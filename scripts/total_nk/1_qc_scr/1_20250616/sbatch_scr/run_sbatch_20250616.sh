9ae6a7c05cdee36244bfd14546a3d6acf4683999
sphilmoon <sphilmoon@gmail.com>
2025-07-04 13:52:12 +0100

    fixing merged DEG counts.

--- scripts/total_nk/1_qc_scr/1_20250616/sbatch_scr/run_sbatch_20250616.sh
+++ scripts/total_nk/1_qc_scr/1_20250616/sbatch_scr/run_sbatch_20250616.sh
@@ -14,7 +14,7 @@ module load docker
 # define paths
 HOST_DATA_DIR="/mnt/lustre/RDS-live/moon/ephemeral/NK_project"
 CONTAINER_DATA_DIR="/home"
-R_SCRIPT="/scripts/total_nk/1_qc_scr/1_20250616/2_merged_raw_cell_counts_fraction_plot_TEST.r"
+R_SCRIPT="/scripts/total_nk/1_qc_scr/1_20250616/3a_merged_DEGs_fractions_plot.r"
 
 # run the docker container with the R script with chat gpt.
 docker run --rm \
